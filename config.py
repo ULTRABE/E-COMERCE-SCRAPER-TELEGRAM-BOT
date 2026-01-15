c92c1ca778407eee2bf4ff9873254840c0c5f3c8
ULTRABE <musklogo2@gmail.com>
2026-01-15 11:08:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))  # your Telegram ID
+
+COOKIE_PATH = "cookies/terabox.json"
